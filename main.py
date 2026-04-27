9b40c878422762b0ea0c70921a600b7648051c57
klan619 <kaylanmccarthy@hotmail.com>
2026-04-27 15:24:46 +0200

    Asteroids V1.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 from constants import *
 from logger import log_state
 from player import Player
@@ -7,6 +8,8 @@ from asteroidfield import AsteroidField
 from asteroid import Asteroid
 from logger import log_event
 from shot import Shot
+
+state = "MENU"
 
 def main():
 
@@ -20,7 +23,72 @@ def main():
 
     dt = 0
 
+    score = 0
+
+    lives = 3
+
+    HIGHSCORE_FILE = "highscore.txt"
+
+    def load_high_score():
+        if not os.path.exists(HIGHSCORE_FILE):
+            return 0
+        with open(HIGHSCORE_FILE, "r") as f:
+            try:
+                return int(f.read())
+            except ValueError:
+                return 0
+            
+    def save_high_score(new_high_score):
+        with open(HIGHSCORE_FILE, "w") as f:
+            f.write(str(new_high_score))
+
+
+    high_score = load_high_score()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.font.init()
+    font = pygame.font.SysFont("monospace", 35)
+
+
+    def draw_menu():
+        screen.fill((0, 0, 0))
+        title_surf = font.render("ASTEROIDS", True, (255, 255, 255))
+        high_score_surf = font.render(f"High Score: {high_score}", True, (255, 215, 0))
+        start_surf = font.render("Press 'Enter' to Start", True, (0, 255, 0))
+        exit_surf = font.render("Press 'Escape' to Quit", True, (255, 0, 0))
+        screen.blit(title_surf, (SCREEN_WIDTH/2 - title_surf.get_width()/2, 150))
+        screen.blit(high_score_surf, (SCREEN_WIDTH/2 - high_score_surf.get_width()/2, 250))
+        screen.blit(start_surf, (SCREEN_WIDTH/2 - start_surf.get_width()/2, 400))
+        screen.blit(exit_surf, (SCREEN_WIDTH/2 - exit_surf.get_width()/2, 450))
+        controls = [
+            "",
+            "CONTROLS:",
+            "Rotate: A / D   OR   Left & Right Arrow Keys",
+            "Thrust: W / S   OR   Up & Down Arrow Keys",
+            "Shoot: Space",
+        ]
+
+        for i, line in enumerate(controls):
+            ctrl_surf = font.render(line, True, (200, 200, 200))
+            x = SCREEN_WIDTH/2 - ctrl_surf.get_width()/2
+            y = 500 + (i * 50) 
+            screen.blit(ctrl_surf, (x, y))
+
+        pygame.display.flip()
+
+    def handle_menu_events():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    global state
+                    state = "GAME"
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -32,40 +100,93 @@ def main():
     Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
+
+    def reset_game():
+        nonlocal score, lives, player
+        global state
+
+        score = 0
+
+        lives = 3
+
+        for sprite in updatable:
+            sprite.kill()
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+        AsteroidField()
+
+        state = "MENU"
     
     
     while True:
-        log_state()
+
+        dt = timer.tick(60) / 1000
+
+        if state == "MENU":
+            handle_menu_events()
+            draw_menu()
+            continue
+        elif state == "GAME":
+
+            log_state()
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    save_high_score(high_score)
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        save_high_score(high_score)
+                        reset_game()
         
-        screen.fill("black")
+            screen.fill("black")
 
-        updatable.update(dt)
+            updatable.update(dt)
 
-        for obj in asteroids:
-            if player.collides_with(obj):
-                log_event("player_hit")
-                print("Game over!")
-                sys.exit()
+            for obj in asteroids:
+                if player.collides_with(obj):
+                    log_event("player_hit")
+                    if lives > 1:
+                        lives -= 1
+                        player.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+                        player.velocity = pygame.Vector2(0, 0)
+                        for a in asteroids:
+                            a.kill()
 
-        for asteroid in asteroids:
-            for shot in shots:
-                if shot.collides_with(asteroid):
-                    log_event("asteroid_shot")
-                    asteroid.split()
-                    shot.kill()
+                    else:
+                        save_high_score(high_score)
+                        reset_game()
+
+            for asteroid in asteroids:
+                for shot in shots:
+                    if shot.collides_with(asteroid):
+                        log_event("asteroid_shot")
+                        if asteroid.radius <= ASTEROID_MIN_RADIUS:
+                            score += 250
+                        else:
+                            score += 100
+                        if score > high_score:
+                            high_score = score
+
+                        asteroid.split()
+                        shot.kill()
 
             
 
-        for thing in drawable:
-            thing.draw(screen)
+            for thing in drawable:
+                thing.draw(screen)
+                score_surface = font.render(f"Score: {score}", True, (255, 255, 255))
+                screen.blit(score_surface, (10, 10))
+                lives_surface = font.render(f"Lives: {lives}", True, (255, 255, 255))
+                screen.blit(lives_surface, (10, 50))
+                high_score_surface = font.render(f"High Score: {high_score}", True, (255, 215, 0))
+                x_pos = SCREEN_WIDTH - high_score_surface.get_width() - 10
+                screen.blit(high_score_surface, (x_pos, 10))
 
-        pygame.display.flip()
 
-        dt = timer.tick(60) / 1000
+            pygame.display.flip()
         
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -43,6 +43,14 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        if keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)
 
 
     def move(self, dt):
